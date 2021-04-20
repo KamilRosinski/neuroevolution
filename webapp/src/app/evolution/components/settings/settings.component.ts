540714d6294fa8d05c2ec7da3337c92a192532e0
Kamil Rosinski <kamilrosiczka@gmail.com>
2021-04-20 22:41:07 +0200

    Create evolution form

--- webapp/src/app/evolution/components/settings/settings.component.ts
+++ webapp/src/app/evolution/components/settings/settings.component.ts
@@ -1,11 +1,11 @@
 import {Component} from '@angular/core';
-import {FormBuilder, FormControl, Validators} from '@angular/forms';
+import {AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Store} from '@ngrx/store';
 import {Router} from '@angular/router';
 import * as EvolutionActions from '../../state/evolution.actions';
 import * as EvolutionSelectors from '../../state/evolution.selectors';
 import {EvolutionState} from '../../state/evolution.state';
-import {Settings} from '../../model/settings';
+import {ActivationFunction, LayerSettings, Settings} from '../../model/settings';
 import {take} from 'rxjs/operators';
 
 @Component({
@@ -14,12 +14,55 @@ import {take} from 'rxjs/operators';
 })
 export class SettingsComponent {
 
+  readonly activationFunction = ActivationFunction;
+
   readonly form = this.formBuilder.group({
-    range: this.formBuilder.control(null, Validators.required)
+    range: this.formBuilder.control(null, Validators.required),
+    game: this.formBuilder.group({
+      fieldWidth: this.formBuilder.control(null),
+      fieldHeight: this.formBuilder.control(null),
+      snakeEndurance: this.formBuilder.control(null),
+    }),
+    evolution: this.formBuilder.group({
+      generationSize: this.formBuilder.control(null),
+      mutationProbability: this.formBuilder.control(null),
+      mutationRate: this.formBuilder.control(null),
+    }),
+    neuralNetwork: this.formBuilder.group({
+      hiddenLayers: this.formBuilder.array([])
+    })
   });
 
   get rangeControl(): FormControl {
     return this.form.get('range') as FormControl;
+  }
+
+  get fieldWidthControl(): FormControl {
+    return this.form.get('game.fieldWidth') as FormControl;
+  }
+
+  get fieldHeightControl(): FormControl {
+    return this.form.get('game.fieldHeight') as FormControl;
+  }
+
+  get enduranceControl(): FormControl {
+    return this.form.get('game.snakeEndurance') as FormControl;
+  }
+
+  get generationSizeControl(): FormControl {
+    return this.form.get('evolution.generationSize') as FormControl;
+  }
+
+  get mutationProbabilityControl(): FormControl {
+    return this.form.get('evolution.mutationProbability') as FormControl;
+  }
+
+  get mutationRateControl(): FormControl {
+    return this.form.get('evolution.mutationRate') as FormControl;
+  }
+
+  get hiddenLayersControl(): FormArray {
+    return this.form.get('neuralNetwork.hiddenLayers') as FormArray;
   }
 
   constructor(private readonly formBuilder: FormBuilder,
@@ -30,15 +73,41 @@ export class SettingsComponent {
       take(1)
     ).subscribe((settings: Settings) => {
       if (settings) {
-        this.form.setValue(settings);
+        this.form.patchValue(settings);
+        settings.neuralNetwork.hiddenLayers.forEach((layer: LayerSettings) => {
+          this.addHiddenLayer(layer);
+        });
       }
     });
   }
 
+  private createLayerControl(value?: LayerSettings): FormGroup {
+    return this.formBuilder.group({
+      numberOfNeurons: this.formBuilder.control(value?.numberOfNeurons),
+      activationFunction: this.formBuilder.control(value?.activationFunction)
+    });
+  }
+
+  addHiddenLayer(value?: LayerSettings): void {
+    this.hiddenLayersControl.push(this.createLayerControl(value));
+  }
+
+  deleteHiddenLayer(index: number): void {
+    this.hiddenLayersControl.removeAt(index);
+  }
+
+  getNumberOfNeurons(layer: AbstractControl): FormControl {
+    return layer.get('numberOfNeurons') as FormControl;
+  }
+
+  getActivationFunction(layer: AbstractControl): FormControl {
+    return layer.get('activationFunction') as FormControl;
+  }
+
   createEvolution(): void {
-    const settings: Settings = this.form.value;
-    this.store.dispatch(EvolutionActions.createEvolution({settings}));
-    this.router.navigate(['evolution', 'overview']).then();
+    this.store.dispatch(EvolutionActions.createEvolution({
+      settings: this.form.value
+    }));
   }
 
 }
--- webapp/src/app/evolution/model/settings.ts
+++ webapp/src/app/evolution/model/settings.ts
@@ -1,3 +1,32 @@
 export interface Settings {
   range: number;
+  game: GameSettings;
+  evolution: EvolutionSettings;
+  neuralNetwork: NeuralNetworkSettings;
+}
+
+export interface GameSettings {
+  fieldWidth: number;
+  fieldHeight: number;
+  snakeEndurance: number;
+}
+
+export interface EvolutionSettings {
+  generationSize: number;
+  mutationProbability: number;
+  mutationRate: number;
+}
+
+export interface NeuralNetworkSettings {
+  hiddenLayers: LayerSettings[];
+}
+
+export interface LayerSettings {
+  numberOfNeurons: number;
+  activationFunction: ActivationFunction;
+}
+
+export enum ActivationFunction {
+  RE_LU = 'ReLU',
+  SIGMOID = 'sigmoid'
 }
